68a36261d30235bc3756126b5fc2d657cc2757ce
junjun <junjun@baicizhan.com>
2018-04-27 23:20:24 +0800

    fx

--- app/models/package.rb
+++ app/models/package.rb
@@ -34,6 +34,6 @@ class Package < ActiveRecord::Base
   def finished(user_id)
     video_ids = self.videos.map{|item|item.id}
     finished_video_ids = UserVideo.where(:user_id=>user_id,:uvtype => UserVideo::TypeWatched).pluck(:video_id)
-    return finished_video_ids.size > 0 && (finished_video_ids & video_ids).sort == finished_video_ids.sort
+    return finished_video_ids.size > 0 && (finished_video_ids & video_ids).sort == video_ids.sort
   end
 end
